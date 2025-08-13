1db00fddb34fdc2a77897a8cfebaba445557319f
Nikita Muromtsev <grafniksan@gmail.com>
2025-08-13 12:39:54 +0400

    Display correct vote stats for MCQ results

--- app/avatars/__init__.py
+++ app/avatars/__init__.py
@@ -13,6 +13,11 @@ from app.settings import settings
 
 AVATAR_SIZE = 640
 
+try:  # pragma: no cover
+    from rlottie_python import LottieAnimation  # type: ignore
+except Exception:  # pragma: no cover
+    LottieAnimation = None  # type: ignore
+
 from .utils import _resize_fit_rgba  # noqa: E402
 from .emoji import _emoji_avatar, _render_emoji_from_font  # noqa: E402
 from .sticker import _sticker_avatar, _render_tgs_high_quality  # noqa: E402
